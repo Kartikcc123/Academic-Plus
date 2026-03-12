8e9f03fbbddcdf72fa3468c76d4c6f50bb663d6c
Kartikcc123 <kartikagarwal4256@gmail.com>
2026-03-12 19:32:01 +0530

    Initial Android TWA project

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -139,9 +139,9 @@ export default function Contact() {
               <div style={{ borderRadius: 24, overflow: 'hidden', minHeight: 420, background: '#e7e0d3' }}>
                 <iframe
                   title="Academic Plus campus map"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3606.311317130635!2d74.63660000000001!3d25.352400000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3968c24f5a8cb5b1%3A0x6b29d47262c5c3b1!2sR.%20C.%20Vyas%20Colony%2C%20Bhilwara%2C%20Rajasthan%20311001!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin"
-                  width="100%"
-                  height="420"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d450.65685125325444!2d74.63430672153342!3d25.36277524398285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3968c3d923b6f479%3A0xfc4d53bbc53b079e!2sAcademic%20Plus!5e0!3m2!1sen!2sin!4v1773323262131!5m2!1sen!2sin"
+                  width="600"
+                  height="450"
                   style={{ border: 0 }}
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
